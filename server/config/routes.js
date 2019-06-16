396402880ab3d8d9c5d4e031536ad43373c57e4b
Ka <kawong2020@u.northwestern.edu>
2019-06-16 18:35:06 -0500

    stream random sounds

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,18 +5,18 @@ module.exports = function (client) {
     client.on("message", (message) => {
         if (Object.keys(config.responseObject).some(word => message.content.toLowerCase().includes(word)))
             message.channel.send(config.responseObject[message.content]);
-
+    
         // Exit and stop if prefix is not there or triggered by another bot
         if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
+    
         if (message.content.startsWith(config.prefix + "ping")) {
             message.channel.send("pong!");
         }
-
+    
         if (message.content.startsWith(config.prefix + "slap")) {
             message.channel.send("this booty");
         }
-
+      
         if (message.content.startsWith(config.prefix + "join")) {
             if (!message.member.voiceChannel) {
                 message.reply("You must be in a voice channel to summon me!");
@@ -25,12 +25,29 @@ module.exports = function (client) {
             message.member.voiceChannel.join().then(connection => {
                 // Yay, it worked!
                 console.log("Successfully connected.");
+                let sound = config.sounds[Math.floor(Math.random()*config.sounds.length)];
+                let streamOptions = {
+                  volume:2,
+                  passes:5
+                }
+                const dispatcher = connection.playArbitraryInput(sound, streamOptions);
+                dispatcher.setVolume(1);
+                dispatcher.on('end', () => {
+                  // The song has finished
+                  console.log('done');
+                  message.guild.voiceConnection.disconnect();
+                });
+
+                dispatcher.on('error', e => {
+                  // Catch any errors that may arise
+                  console.log(e);
+                });
             }).catch(e => {
                 // Oh no, it errored! Let's log it to console :)
                 console.error(e);
             });
         }
-
+      
         if (message.content.startsWith(config.prefix + "leave")) {
             if (message.guild.voiceConnection) {
                 message.guild.voiceConnection.disconnect();
@@ -43,7 +60,7 @@ module.exports = function (client) {
         if (message.content.startsWith(config.prefix + "clearchat")) {
             async function clear() {
                 message.delete();
-                const fetched = await message.channel.fetchMessages({ limit: 99 });
+                const fetched = await message.channel.fetchMessages({limit: 99});
                 message.channel.bulkDelete(fetched);
             }
             clear();
